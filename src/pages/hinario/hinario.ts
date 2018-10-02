9cc8a35d236c4dd79eaa9b1589d42284509313e1
Lucas <queiroz.lucas@hotmail.com>
2018-10-01 22:58:58 -0300

    load hino

--- src/pages/hinario/hinario.ts
+++ src/pages/hinario/hinario.ts
@@ -1,30 +1,52 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController
+} from 'ionic-angular';
+
+import { Hino } from '../../models/hino.model';
+
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  CollectionReference
+} from 'angularfire2/firestore';
+import { SelecionaHinoPage } from '../seleciona-hino/seleciona-hino';
+
 
 @IonicPage()
 @Component({
   selector: 'page-hinario',
   templateUrl: 'hinario.html',
 })
-
 export class HinarioPage {
-  hinos: Observable<Hino[]>;
   hinosCollecion: AngularFirestoreCollection<Hino>;
-
+  hinos: Observable<Hino[]>;
 
   constructor(
-    public navParams: NavParams,
     public navCtrl: NavController,
-    private db: AngularFirestore
+    public navParams: NavParams,
+    private db: AngularFirestore,
+    private _loadingCtrl: LoadingController,
+
   ) { }
 
   ionViewWillEnter() {
-    this.hinosCollecion = this.db.collection<Hino>('/hinos',
-    (ref: CollectionReference) => ref
-      .orderBy('numero', 'asc')
-      .orderBy('titulo', 'asc'));
+    let loading = this._loadingCtrl.create({
+      content: 'Carregando Hinos...'
+    });
+    loading.present();
 
-    this.hinos = this.hinosCollecion.valueChanges();
+    this.hinosCollecion = this.db.collection<Hino>('/hinos',
+      (ref: CollectionReference) => ref
+        .orderBy('numero', 'asc')
+        .orderBy('titulo', 'asc'));
+
+    this.hinos = this.hinosCollecion.valueChanges()
+    loading.dismiss();
   }
 }
